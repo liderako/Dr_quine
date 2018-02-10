ada4c4a3aebbb266de00d7d4f241623add4a4a3a
liderako <trotillostudio@gmail.com>
2018-02-10 19:41:47 +0200

    Create Colleen.c

---
+++ Colleen.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+/*
+	inside main
+*/
+int main(){char *s="#include <stdio.h>%c/*%c	inside main%c*/%cint main(){char *s=%c%s%c;printf(s,10,10,10,10,34,s,34,10);}%c";printf(s,10,10,10,10,34,s,34,10);}
