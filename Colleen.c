53627d2f773b6d4cf1cdaa81a653b317382c0599
liderako <trotillostudio@gmail.com>
2018-02-10 19:57:34 +0200

    Update Colleen.c

--- Colleen.c
+++ Colleen.c
@@ -2,4 +2,9 @@
 /*
 	inside main
 */
-int main(){char *s="#include <stdio.h>%c/*%c	inside main%c*/%cint main(){char *s=%c%s%c;printf(s,10,10,10,10,34,s,34,10);}%c";printf(s,10,10,10,10,34,s,34,10);}
+void	call(){return;}
+int main(){
+/*
+	text in main
+*/
+call();char *s="#include <stdio.h>%c/*%c	inside main%c*/%cvoid	call(){return;}%cint main(){%c/*%c	text in main%c*/%ccall();char *s=%c%s%c;printf(s,10,10,10,10,10,10,10,10,10,34,s,34,10);}%c";printf(s,10,10,10,10,10,10,10,10,10,34,s,34,10);}
